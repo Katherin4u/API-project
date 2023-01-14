1e578bf0d7f998e8525778949778bfe93d0de748
katherin <katherin.garciaVA1@gmail.com>
2023-01-13 19:48:32 -0500

    login modal auth 4 frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/loginFromPage/index";
 import SignupFormPage from "./components/SignupFormPage/index";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation/index";
@@ -20,9 +19,6 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
